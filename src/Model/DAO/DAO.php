9a6598c53593bb227c9ac171ebcc97729098871e
JoaoNeto8080 <ljoao6250@gmail.com>
2023-10-23 19:55:50 -0300

    Update DAO.php

--- src/Model/DAO/DAO.php
+++ src/Model/DAO/DAO.php
@@ -9,7 +9,7 @@ class DAO{
 
     public function __construct()
     {
-        $this->conexao = new PDO("mysql:host=localhost;port=3308;dname=banco_php",
+        $this->conexao = new PDO("mysql:host=localhost;port=3308;dname=empresa_junior",
                                 "root","");    
     }
     public function getConexao()
