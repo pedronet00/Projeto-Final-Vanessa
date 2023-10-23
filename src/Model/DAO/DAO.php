a8889509891904f4a745d17362cd6f86f266b60c
JoaoNeto8080 <ljoao6250@gmail.com>
2023-10-23 19:59:39 -0300

    Revert "Update DAO.php"
    This reverts commit 9a6598c53593bb227c9ac171ebcc97729098871e.

--- src/Model/DAO/DAO.php
+++ src/Model/DAO/DAO.php
@@ -9,7 +9,7 @@ class DAO{
 
     public function __construct()
     {
-        $this->conexao = new PDO("mysql:host=localhost;port=3308;dname=empresa_junior",
+        $this->conexao = new PDO("mysql:host=localhost;port=3308;dname=banco_php",
                                 "root","");    
     }
     public function getConexao()
