1bd19c435a33e56dad53ed29bca26693cc07b371
JoaoNeto8080 <ljoao6250@gmail.com>
2023-10-23 22:07:11 -0300

    Update

---
+++ src/Model/Entity/Funcoes.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Php\Empresajrtoledo\Model\Entity;
+
+class Funcoes{
+    private $id;
+    private $Nome;
+    private $Curso;
+    private $Cargo;
+    private $Foto;
+
+
+
+    public function __construct($id, $Nome, $Curso, $Cargo, $Foto)
+    {
+        $this->setId($id);
+        $this->setNome(($Nome)); 
+        $this->setCurso($Curso);
+        $this->setCargo(($Cargo)); 
+        $this->setFoto($Foto);
+    }
+    public function setId($id){
+
+        $this->id = $id;
+    }
+
+    public function setNome($Nome){
+        $this->Nome = $Nome;
+    }
+    public function setCurso($Curso){
+        $this->Curso = $Curso;
+    }
+    public function setCargo($Cargo){
+        $this->Cargo = $Cargo;
+    }
+    public function setFoto($Foto){
+        $this->Foto = $Foto;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+    public function getNome(){
+        return $this->Nome;
+    }
+    public function getCurso(){
+        return $this->Curso;
+    }
+    public function getCargo(){
+        return $this->Cargo;
+    }
+    public function getFoto(){
+        return $this->Foto;
+    }
+}
