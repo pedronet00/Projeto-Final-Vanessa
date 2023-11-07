8386f2a792a7dfe7608544b1f014c9d922ca3151
LuOliver2323 <128651258+LuOliver2323@users.noreply.github.com>
2023-11-06 22:00:20 -0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,39 @@ $path = $_SERVER['PATH_INFO'] ?? '/';
 
 $router = new \Php\Empresajrtoledo\Router($method, $path);
 
+
+$router->get("/Parceiro/parceiro",
+    'Php\Empresajrtoledo\Controller\ParceiroController@parceiro'
+);
+
+$router->get("/Parceiro/index",
+    'Php\Empresajrtoledo\Controller\ParceiroController@index'
+);
+
+$router->get("/Parceiro/inserir",
+    'Php\Empresajrtoledo\Controller\ParceiroController@inserir'
+);
+
+$router->get("/Parceiro/alterar/{id}",
+    'Php\Empresajrtoledo\Controller\ParceiroController@alterar'
+);
+
+$router->get("/Parceiro/excluir/{id}",
+    'Php\Empresajrtoledo\Controller\ParceiroController@excluir'
+);
+
+$router->post("/Parceiro/gravar",
+    'Php\Empresajrtoledo\Controller\ParceiroController@gravar'
+);
+
+$router->post("/Parceiro/deletar/{id}",
+    'Php\Empresajrtoledo\Controller\ParceiroController@deletar'
+);
+
+$router->post("/Parceiro/editar/{id}",
+    'Php\Empresajrtoledo\Controller\ParceiroController@editar'
+);
+
 $resultado = $router->handler();
 
 if (!$resultado){
